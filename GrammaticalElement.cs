ecc96931adb305203f646d46a233939bf2333daf
Natito <stiven.guerra@udea.edu.co>
2019-09-26 09:06:33 -0500

    firstCommit

---
+++ GrammaticalElement.cs
@@ -0,0 +1,5 @@
+public class GrammaticalElement
+{
+    private bool isNonTerminal;
+    private string symbol;
+}
---
+++ GrammaticalProduction.cs
@@ -0,0 +1,8 @@
+import System.collections;
+
+public class GrammaticalProduction
+{
+    private GrammaticalElement leftSide;
+    private List<GrammaticalElement> rightSide;
+    
+}
